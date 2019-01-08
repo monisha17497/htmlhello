ac0ce9d119a332a2183fa85f6b6f774076c81f8c
monisha17497 <monisha.mathivanan@gmail.com>
2019-01-08 14:31:56 +0530

    amstrong

---
+++ amstrong/amstrong/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amstrong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(amstrong(153));
+        }
+        public static bool amstrong(int n)
+        {
+            bool Flag = true;
+            int count;
+            count = (int)(Math.Log10(n) + 1);
+            double sum = 0;
+            int rem;
+            int temp = n;
+            while (n != 0)
+            {
+                rem = n % 10; ;
+                sum = sum + (Math.Pow(rem, count));
+                n = n / 10;
+            }
+            if (sum == temp)
+            {
+                return Flag;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
